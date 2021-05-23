549403d76a6f124829b5eef586951d00994bb194
UniquePants <6rutzen@informatik.uni-hamburg.de>
2021-05-24 01:29:47 +0200

    quick fix

--- Assets/Groups/Group W/Scripts/ActionPhase.cs
+++ Assets/Groups/Group W/Scripts/ActionPhase.cs
@@ -230,11 +230,15 @@ public class ActionPhase : MonoBehaviour
     // throws the equipped weapon from activePlayer to targetPlayer
     IEnumerator ThrowWeapon(PlayerProperties activePlayer, PlayerProperties targetPlayer, Action onComplete)
     {
-        print("now throwing weapon");
+        print("is back row; now throwing weapon");
         GameObject throwableWeapon = SpawnNewWeapon(activePlayer.CurrentRowPosition, activePlayer.weapon);
 
-        // should get a rigidbody to be able to throw it
-        throwableWeapon.AddComponent<Rigidbody>();
+        // should get a rigidbody to be able to throw it, if not already available
+        if(throwableWeapon.GetComponent<Rigidbody>() != null)
+        {
+            throwableWeapon.AddComponent<Rigidbody>();
+        }
+        
         // following code is adapted from https://gist.github.com/marcelschmidt1337/e46d166b639c06af3ba896fcb8412be4
         float throwAngle = 45.0f;
         float gravity = 9.8f;
